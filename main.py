8230b6a64e82e280213f3b8c316ae592df960d26
John Dungao <johnmiguel.dungao@gmail.com>
2022-12-30 10:00:35 -0500

    blackjack done

--- main.py
+++ main.py
@@ -1,20 +1,96 @@
-from art import logo
 import random
+from art import logo
 
-deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-user_cards = []
+playerIn = True
+dealerIn = True
+playerContinue = True
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A']
+playerHand = []
+dealerHand = []
 
-def get_card(players):
-    random_card = deck[random.randint(0, 13)]
-    return players.append(random_card)
+print(logo)
+def dealCard(turn):
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
 
 
-
-def compute_card(player):
-    total = sum(user_cards)
+# calculate the total of each card
+def computeCard(turn):
+    total = 0
+    face = ['J', 'K', 'Q']
+    for card in turn:
+        if card in range(1, 11):
+            total += card
+        elif card in face:
+            total += 10
+        else:
+            if total > 11:
+                total += 1
+            else:
+                total += 11
     return total
 
-game_continue = True
 
-while game_continue:
-    print(f"Your cards: {for}")
+def revealDealerHand():
+    if len(dealerHand) == 2:
+        return dealerHand[0]
+    elif len(dealerHand) > 2:
+        return dealerHand[0], dealerHand[1]
+
+
+for _ in range(2):
+    dealCard(dealerHand)
+    dealCard(playerHand)
+
+while playerContinue:
+    while playerIn or dealerIn:
+        print(f"Dealer had {revealDealerHand()} and X")
+        print(f"You have {playerHand} for a total of {computeCard(playerHand)}")
+        if playerIn:
+            stayOrHit = input("1: Stay\n2: Hit\n")
+        if computeCard(dealerHand) > 17:
+            dealerIn = False
+        else:
+            dealCard(dealerHand)
+        if stayOrHit == '1':
+            playerIn = False
+        else:
+            dealCard(playerHand)
+        if computeCard(playerHand) >=21:
+            break
+        elif computeCard(dealerHand) >= 21:
+            break
+
+    if computeCard(playerHand) == 21:
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("Blackjack! You win:")
+    elif computeCard(dealerHand) == 21:
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("Blackjack! Dealer wins!")
+    elif computeCard(playerHand) > 21:
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("You bust! Dealer Wins")
+    elif computeCard(dealerHand) > 21:
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("Dealer busts! You win!")
+    elif 21 - computeCard(dealerHand) < 21 - computeCard(playerHand):
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("Dealer wins!")
+    elif 21 - computeCard(dealerHand) > 21 - computeCard(playerHand):
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("Player wins!")
+    elif computeCard(dealerHand) == computeCard(playerHand):
+        print(f"\nYou have {playerHand} for a total of {computeCard(playerHand)} and the dealer has {dealerHand} for a total of {computeCard(dealerHand)}")
+        print("PUSH")
+    user_choice = input("Do you still want to play? press 1 for 'yes' or press 2 for 'no' ")
+    if user_choice == '2':
+        playerContinue = False
+        print("Thanks for playing!")
+    else:
+        dealerHand = []
+        playerHand = []
+        for _ in range(2):
+            dealCard(dealerHand)
+            dealCard(playerHand)
