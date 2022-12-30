1a2472fb89caf66476e5a738d95d93e405b4e2d7
John Dungao <johnmiguel.dungao@gmail.com>
2022-12-30 01:04:11 -0500

    added

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
 from art import logo
 import random
 
-random_choice = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "K", "Q", "J"]
-deck = {
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "A": 11,
-    "K": 10,
-    "Q": 10,
-    "J": 10
-}
-user_cards = {}
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+user_cards = []
+
+def get_card(players):
+    random_card = deck[random.randint(0, 13)]
+    return players.append(random_card)
+
+def compute_card():
 
 
-def get_card():
-    card_face = random_choice[random.randint(0, 13)]
-    random_card = deck[card_face]
-    if random_card >= 10:
-        if card_face == 'A':
-            user_cards[card_face] = random_card
-        elif card_face == 'K':
-            user_cards[card_face] = random_card
-        elif card_face == 'Q':
-            user_cards[card_face] = random_card
-        elif card_face == 'J':
-            user_cards[card_face] = random_card
-        else:
-            user_cards[card_face] = random_card
-    user_cards[card_face] = random_card
+
+print(f"Your cards: [{user_card_one}, {user_card_two}]")
